0ae0c0253ed75ab494c9e2090975782ce14d8372
Shahd-Ibrahiem <shahdibrahiem2004@gmail.com>
2026-05-09 16:06:51 +0300

    RAG System

---
+++ app/rag/vector_store.py
@@ -0,0 +1,11 @@
+from langchain_community.vectorstores import FAISS
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.document_loaders import TextLoader
+
+def load_vectorstore():
+    loader = TextLoader("data/business_knowledge.txt")
+    docs = loader.load()
+
+    embeddings = OllamaEmbeddings(model="llama3")
+
+    return FAISS.from_documents(docs, embeddings)
